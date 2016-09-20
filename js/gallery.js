d45043b2283fbcce86fddc9600ee6d5a4fc535ba
rjpacket <jimbo922@163.com>
2016-09-20 12:32:00 +0800

    Site updated: 2016-09-20 12:31:59

--- js/gallery.js
+++ js/gallery.js
@@ -2,15 +2,13 @@
   // Caption
   $('.entry').each(function(i){
     $(this).find('img').each(function(){
-      if (!$(this).parent().is("a")) {
-        var alt = this.alt, title = this.title;
-        var caption = title ? title : alt;
-        if (caption){
-          $(this).after('<div class="caption"><i  class="fa fa-picture-o"></i>' + caption + '</div>');
-        }
+      var alt = this.alt;
 
-        $(this).wrap('<a href="' + this.src + '" title="' + caption + '" class="fancybox" rel="gallery' + i + '" />');
+      if (alt){
+        $(this).after('<span class="caption">' + alt + '</span>');
       }
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox" rel="gallery' + i + '" />');
     });
   });
 
